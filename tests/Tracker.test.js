3199f0784df0137b1cfd120eed5cd532e1316697
jehling <jehling@users.noreply.github.com>
2021-05-07 23:01:36 -0400

    built out basic tests for tracker

--- tests/Tracker.test.js
+++ tests/Tracker.test.js
@@ -1,5 +1,50 @@
-const Tracker = require('../src/Tracker');
+// Testing Class
+const { Tracker } = require('../src/Tracker');
 
-test('test test', () => {
-    expect(1+2).toBe(3);
+// Testing Constants
+let tracker;
+const testALID = '1';
+const testTitleObj = {
+    english: 'Cowboy Beebop',
+    romaji: 'COWBOY BEEBOP'
+};
+
+// Mocks
+
+// Tests
+describe('Tracker Suite', () => {
+    // SETUP & TEARDOWN
+    beforeEach(() => {
+        // TODO mock reset logic
+        tracker = new Tracker();
+        tracker.trackedMediaIds.set(testALID, testTitleObj);
+    });
+    
+    // TESTS
+    test('getMediaIds', () => {
+        expect(tracker.getMediaIds()).toStrictEqual([testALID]);
+        tracker.trackedMediaIds.clear();
+        expect(tracker.getMediaIds()).toHaveLength(0);
+    });
+
+    test('hasMediaId', () => {
+        expect(tracker.hasMediaId(testALID)).toBeTruthy();
+        tracker.trackedMediaIds.clear();
+        expect(tracker.hasMediaId(testALID)).toBeFalsy();
+    });
+
+    test('isValidMediaId', () => {
+        expect(tracker.isValidMediaId('abc')).toBeFalsy();
+        expect(tracker.isValidMediaId(0)).toBeFalsy();
+        expect(tracker.isValidMediaId(-1)).toBeFalsy();
+        expect(tracker.isValidMediaId(testALID)).toBeTruthy();
+    });
+
+    test('getShowTitle', () => {
+        expect(tracker.getShowTitle(testALID)).toEqual(testTitleObj.english);
+        let tracker2 = new Tracker();
+        tracker2.trackedMediaIds.set(testALID, { romaji: testTitleObj.romaji });
+        expect(tracker2.getShowTitle(testALID)).toEqual(testTitleObj.romaji);
+    });
 });
+
